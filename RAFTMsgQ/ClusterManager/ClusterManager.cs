547c887a97c704e762daef99684530908f062458
Roger Xi <roger.xi.1984@gmail.com>
2020-02-11 23:42:43 +0800

    commit_2020-02-11

---
+++ RAFTMsgQ/ClusterManager/ClusterManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace CarrotStore.ClusterManager
+{
+    class ClusterManager
+    {
+        private Dictionary<IPAddress, string> _nodeList = new Dictionary<IPAddress, string>();
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public void Initialize()
+        {
+            if (_nodeList.Count < 3)
+                return;
+
+            // call init API in each node
+        }
+
+        public void AddServer(IPAddress ip, string serverName)
+        {
+            _nodeList.Add(ip, serverName);
+        }
+
+        public void RemoveServer(IPAddress ip)
+        {
+            _nodeList.Remove(ip);
+        }
+    }
+}
--- RAFTMsgQ/MsgQCore/Node.cs
+++ RAFTMsgQ/MsgQCore/Node.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
+using System.Collections.Generic;
 
 namespace CarrotStore.MsgQCore
 {
     public class Node
     {
+        private bool _initialized = false;
         private Dictionary<IPAddress, string> _nodeList = null;
 
         public Guid Identity { get; private set; }
-        
+
         public string Name { get; private set; }
 
         public IPAddress IP { get; private set; }
@@ -21,9 +22,20 @@ namespace CarrotStore.MsgQCore
             this._nodeList = new Dictionary<IPAddress, string>();
         }
 
-        public void Initialize()
+        public void Initialize(Dictionary<IPAddress, string> nodeList)
         {
+            object obj = new object();
 
+            lock (obj)
+            {
+                if (!this._initialized)
+                {
+                    this._initialized = true;
+                    this._nodeList = nodeList;
+
+                    this.RequestVote();
+                }
+            }
         }
 
         public void RequestVote()
