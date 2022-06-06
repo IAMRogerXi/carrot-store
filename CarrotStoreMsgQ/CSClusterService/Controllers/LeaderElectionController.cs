01cd9a46bad15a8e94a8aa072f51afb2fa705086
Roger Xi 😁 <roxi@microsoft.com>
2022-06-06 19:10:53 +0800

    2022-06-06

---
+++ CarrotStoreMsgQ/CSClusterService/Controllers/LeaderElectionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSClusterService.Controllers
+{
+    public class LeaderElectionController : Controller
+    {
+        public IActionResult AckLeaderElection()
+        {
+            return View();
+        }
+
+        public IActionResult Voting()
+        {
+            return View();
+        }
+    }
+}
