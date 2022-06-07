0203e87a06218459bd4e447cc5b7c5cd3360aa8c
Roger Xi 😁 <roxi@microsoft.com>
2022-06-07 11:51:21 +0800

    2022-06-07

--- CarrotStoreMsgQ/CSClusterService/WorkerProcessor/TimerJob.cs
+++ CarrotStoreMsgQ/CSClusterService/WorkerProcessor/TimerJob.cs
@@ -1,10 +1,34 @@
-﻿namespace CSClusterService.WorkerProcessor
+﻿using ClusterContract;
+
+namespace CSClusterService.WorkerProcessor
 {
     public class TimerJob : BackgroundService
     {
+        ICluster currentCluster;
+
+        INode currentNode;
+
+        IHeartbeatService heartbeatService;
+
+        public TimerJob(ICluster cluster, INode node, IHeartbeatService heartbeatService)
+        {
+            this.currentCluster = cluster;
+            this.currentNode = node;
+            this.heartbeatService = heartbeatService;
+        }
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            throw new NotImplementedException();
+            if (currentNode.IsLeader)
+            {
+                heartbeatService.SendHeartbeatAsync(currentCluster, stoppingToken);
+            }
+            else
+            {
+                heartbeatService.MonitorAsync(null, stoppingToken);
+            }
+
+            return null;
         }
     }
 }
