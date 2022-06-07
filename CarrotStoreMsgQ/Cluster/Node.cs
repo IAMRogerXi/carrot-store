42cee9fd574255de7d0b36458e81fafe6172ed8e
Roger Xi 😁 <roxi@microsoft.com>
2022-06-07 13:44:10 +0800

    2022-06-07

---
+++ CarrotStoreMsgQ/Cluster/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClusterContract;
+
+namespace Cluster
+{
+    internal class Node : NodeBase
+    {
+        public Node(IHeartbeatService heartbeatService, ILeaderElectionService leaderElectionService) : base(heartbeatService, leaderElectionService)
+        {
+        }
+
+        public override bool IsLeader => false;
+    }
+}
